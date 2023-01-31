88e59b881de62f2376bf4ea5a98f05d3df84c05c
pavelbezpravel <p.talashchenko@gmail.com>
2023-02-01 01:55:07 +0300

    implement installation script

---
+++ .config/setup/install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+git clone --bare git@github.com:pavelbezpravel/.dotfiles.git "$HOME"/.dotfiles
+
+cfgit() {
+    /usr/bin/git --git-dir="$HOME"/.dotfiles/ --work-tree="$HOME"
+}
+
+mkdir -p .dotfiles-backup
+cfgit checkout
+
+if [ $? = 0 ]; then
+    echo "Checked out dotfiles.";
+else
+    echo "Backing up pre-existing dotfiles.";
+    cfgit checkout 2>&1 | grep -E "\s+\." | awk 'print $1' | xargs -I{} mv {} .dotfiles-backup/{}
+fi;
+
+cfgit checkout
+cfgit config status.showUntrackedFiles no
+
