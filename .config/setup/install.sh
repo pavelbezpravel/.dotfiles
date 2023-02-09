70fb87a28932516928bf088c8f634c6664c88749
pavelbezpravel <p.talashchenko@gmail.com>
2023-02-10 00:53:58 +0300

    code cleanup

--- .config/setup/install.sh
+++ .config/setup/install.sh
@@ -1,22 +1,32 @@
 #!/usr/bin/env sh
 
-cd "$HOME" || exit
-git clone --bare https://github.com/pavelbezpravel/.dotfiles.git "$HOME"/.dotfiles
-
 cfgit() {
     /usr/bin/git --git-dir="$HOME"/.dotfiles/ --work-tree="$HOME" "$@"
 }
 
-cfgit checkout
+install_zsh() {
+    if [ "$(which zsh)" = 0 ]; then
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    fi
+}
 
-if [ $? = 0 ]; then
-    echo "Checked out dotfiles.";
-else
-    echo "Backing up pre-existing dotfiles.";
-    mkdir -p "$HOME"/.dotfiles_backup
-    cfgit checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | xargs -I{} mv {} "$HOME"/.dotfiles_backup/{}
-fi;
+install() {
+    cd "$HOME" || exit
 
-cfgit checkout
-cfgit config --local status.showUntrackedFiles no
+    install_zsh
 
+    git clone --bare https://github.com/pavelbezpravel/.dotfiles.git "$HOME"/.dotfiles
+
+    if [ "$(cfgit checkout)" = 0 ]; then
+        echo "Checked out dotfiles."
+    else
+        echo "Backing up pre-existing dotfiles."
+        mkdir -p "$HOME"/.dotfiles_backup
+        cfgit checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | xargs -I{} mv {} "$HOME"/.dotfiles_backup/{}
+    fi
+
+    cfgit checkout
+    cfgit config --local status.showUntrackedFiles no
+}
+
+install
