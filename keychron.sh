68b574af70acd275799ad111eb158c83d9afc704
pavelbezpravel <p.talashchenko@gmail.com>
2023-02-14 11:23:34 +0300

    upload keychron setup

---
+++ keychron.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# replace <value> below with the one that worked for you in the previous step (0, 1 or 2)
+# example: echo "options hid_apple fnmode=2 | sudo tee /etc/modprobe.d/hid_apple.conf"
+# this will erase any pre-existing contents from /etc/modprobe.d/hid_apple.conf
+echo "options hid_apple fnmode=0" | sudo tee /etc/modprobe.d/hid_apple.conf
+
+# the "-k all" part is not always needed, but it's better to do that for all kernels anyway
+sudo update-initramfs -u -k all
+sudo systemctl reboot
+
+# read: https://gist.github.com/andrebrait/961cefe730f4a2c41f57911e6195e444
+# read: https://github.com/kurgol/keychron/blob/master/k6.md
