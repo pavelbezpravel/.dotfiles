15089322f61220fd4be708dfbd2ab2cd2b574b62
pavelbezpravel <p.talashchenko@gmail.com>
2024-03-01 03:37:39 +0300

    feat: migrate from bare git repo to stow tool

--- .config/setup/install.sh
+++ .config/setup/install.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-cfgit() {
-    git --git-dir="$HOME"/.dotfiles/ --work-tree="$HOME" "$@"
-}
 
 setup_zsh() {
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
@@ -22,20 +19,9 @@ install() {
 
     setup_zsh
 
-    git clone --bare --recurse-submodules https://github.com/pavelbezpravel/.dotfiles.git "$HOME"/.dotfiles
+    git clone --recurse-submodules https://github.com/pavelbezpravel/.dotfiles.git "$HOME"/.dotfiles
 
-    if [ "$(cfgit checkout)" = 0 ]; then
-        echo "Checked out dotfiles."
-    else
-        echo "Backing up pre-existing dotfiles."
-        mkdir -p "$HOME"/.dotfiles_backup
-        cfgit checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | xargs -I{} mv {} "$HOME"/.dotfiles_backup/{}
-    fi
-
-    cfgit checkout
-    cfgit config --local status.showUntrackedFiles no
-    cfgit submodule update --init
-
+    stow --adopt --target="$HOME" --dir="$HOME/.dotfiles" .
     setup_font
     gen_completions
 }
