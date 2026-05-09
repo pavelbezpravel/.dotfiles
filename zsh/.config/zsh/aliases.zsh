008b9fe201c1f56d2c6e9277958d7a3f09722411
pavelbezpravel <p.talashchenko@gmail.com>
2026-05-09 11:37:29 +0300

    zsh: update aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,2 +1,7 @@
 unalias la 2>/dev/null
-command -v eza >/dev/null && alias la="eza -lAgh --icons=always"
+command -v eza >/dev/null \
+  && alias la='eza -lAgh --icons=always'
+
+unalias fp 2>/dev/null
+command -v fzf >/dev/null && command -v bat >/dev/null \
+  && alias fp='fzf -m --preview="bat {}" --bind "enter:become($EDITOR {+})"'
