b49d8294c06e16f41e5eb0010ac7c7eda7c5ae92
pavelbezpravel <p.talashchenko@gmail.com>
2026-05-13 21:48:24 +0300

    zsh: fix rg completions

--- zsh/.config/zsh/completion.zsh
+++ zsh/.config/zsh/completion.zsh
@@ -1,6 +1,6 @@
 if [[ ! -f "$ZSH_CACHE_DIR/completions/_rg" ]] && command -v rg >/dev/null; then
   mkdir -p "$ZSH_CACHE_DIR/completions"
-  rg --generate complete-zsh > "$ZSH_CACHE/completions/_rg"
+  rg --generate complete-zsh > "$ZSH_CACHE_DIR/completions/_rg"
   rm "$ZSH_COMPDUMP"
 fi
 
